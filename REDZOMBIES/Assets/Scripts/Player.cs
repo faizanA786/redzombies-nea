be1b123aef130bef12487bfad930bbd878c027b4
outlaw16 <faizan65650@gmail.com>
2024-01-11 00:13:35 +0000

    minor changes

--- REDZOMBIES/Assets/Scripts/Player.cs
+++ REDZOMBIES/Assets/Scripts/Player.cs
@@ -6,25 +6,26 @@ public class Player : MonoBehaviour
     int playerMaxHealth = 3;
     public int playerHealth;
     public int playerPoints = 0;
-    float shootTimer; //Values are set in Shoot()
+    float shootTimer; //Differing values are set in Shoot()
     public bool playerDeath = false;
-    public int weaponSelected;
+    public int weaponSelected; //Holds the value of the weapon currently assigned
 
-    public GameObject bulletObject; //References bullet object
     public GameObject weaponSprite; //References weapon object
+    public GameObject bulletObject; //References bullet object
     Rigidbody2D rb;
 
     void Start() //Initialisation, called on first frame
     {
         playerHealth = playerMaxHealth;
+        weaponSelected = 1; //Assign pistol as starting weapon
         rb = gameObject.GetComponent<Rigidbody2D>(); //Reference rigidbody component
     }
 
     void Update() //Called every frame
     {
         Movement();
-        Shoot();
         FaceCursor();
+        Shoot();
     }
 
     void Movement() //Responsible for movement of this object
@@ -46,13 +47,14 @@ public class Player : MonoBehaviour
 
     void Shoot() //Responsible for firing a bullet
     {
-        shootTimer *= Time.deltaTime; //Reduces value every second
-        if (Input.GetMouseButton(0) && shootTimer <= 0)
+        shootTimer -= Time.deltaTime; //Reduce the shooting cooldown every second
+        if (Input.GetMouseButton(0) && shootTimer <= 0) //If left mouse button is held down and theres no shoot cooldown
         {
             if (weaponSelected == 1) //1 is pistol
             {
-                Instantiate(bulletObject, weaponSprite.transform.position + new Vector3(0, 0.5f, 0), transform.rotation);
-                shootTimer = 0.7f;
+                Instantiate(bulletObject, weaponSprite.transform.position + new Vector3(0, 0.1f, 0), transform.rotation);
+                //Create instance of bullet at 0.1 pixels infront of weapon sprites position with same rotation as player
+                shootTimer = 0.6f; //Sets a cooldown to how fast the weapon can be fired
             }
 
             /*else if (weaponSelected == 2) //2 is assualt rifle
@@ -60,8 +62,12 @@ public class Player : MonoBehaviour
 
             }*/
 
-            Debug.Log("left mouse button inputted\nFiring bullet!");
+            /*else if (weaponSelected == 3) //3 is shotgun
+            {
 
+            }*/
+            
+            Debug.Log("left mouse button inputted\nFiring bullet!");
         }
     }
 
